52b1ba05966ea7dca9d41b133c99dce7157fc9c5
BeestingGit <anthonypaulchong@gmail.com>
2025-04-15 19:25:49 -0400

    Created login and registration screens

--- lib/screens/registration_page.dart
+++ lib/screens/registration_page.dart
@@ -1,1 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'home_page.dart';
 
+class RegistrationPage extends StatefulWidget {
+  @override
+  _RegistrationPageState createState() => _RegistrationPageState();
+}
+
+class _RegistrationPageState extends State<RegistrationPage> {
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _firstNameController = TextEditingController();
+  final _lastNameController = TextEditingController();
+
+  Future<void> register() async {
+    try {
+      final userCredential = await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(
+            email: _emailController.text,
+            password: _passwordController.text,
+          );
+
+      await FirebaseFirestore.instance
+          .collection("users")
+          .doc(userCredential.user?.uid)
+          .set({
+            "userId": userCredential.user?.uid,
+            "firstName": _firstNameController.text,
+            "lastName": _lastNameController.text,
+            "role": "user",
+            "registrationDate": DateTime.now(),
+          });
+
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (_) => HomePage()),
+      );
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: _firstNameController,
+              decoration: InputDecoration(labelText: "First Name"),
+            ),
+            TextField(
+              controller: _lastNameController,
+              decoration: InputDecoration(labelText: "Last Name"),
+            ),
+            TextField(
+              controller: _emailController,
+              decoration: InputDecoration(labelText: "Email"),
+            ),
+            TextField(
+              controller: _passwordController,
+              decoration: InputDecoration(labelText: "Password"),
+              obscureText: true,
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(onPressed: register, child: Text("Register")),
+          ],
+        ),
+      ),
+    );
+  }
+}
